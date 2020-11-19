556ebd75085a6e0f5bc2397310709a126c3ff151
Gustavo <ing.glaragomez@gmail.com>
2020-11-19 10:16:44 -0700

    modified script and readme file

--- initial-setup.sh
+++ initial-setup.sh
@@ -1,10 +1,14 @@
 cd /
 echo "Changed working directory to /"
+
 echo "Downloading crt-ready-retropie-config.tar.gz..."
 sudo wget https://github.com/tavuntu/crt-ready-retropie/raw/main/crt-ready-retropie-config.tar.gz
+
 echo "Decompressing crt-ready-retropie-config.tar.gz..."
 sudo tar -zxf crt-ready-retropie-config.tar.gz
+
 echo "Deleting crt-ready-retropie-config.tar.gz..."
 sudo rm crt-ready-retropie-config.tar.gz
+
 echo "Restarting system..."
 sudo reboot
